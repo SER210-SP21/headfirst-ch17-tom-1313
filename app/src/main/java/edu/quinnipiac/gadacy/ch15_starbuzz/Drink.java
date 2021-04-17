e53a751697a85a22664769a0253660ef200f92ea
tom-1313 <trgadacy@quinnipiac.edu>
2021-04-16 22:54:50 -0400

    ch16-gadacy

--- app/src/main/java/edu/quinnipiac/gadacy/ch15_starbuzz/Drink.java
+++ app/src/main/java/edu/quinnipiac/gadacy/ch15_starbuzz/Drink.java
@@ -1,5 +1,6 @@
 package edu.quinnipiac.gadacy.ch15_starbuzz;
 
+
 public class Drink {
     private String name; private String description; private int imageResourceId;
     public static final Drink[] drinks = {
@@ -8,6 +9,7 @@ public class Drink {
             new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter)
     };
 
+    //Each Drink has a name, description, and an image resource
     private Drink(String name, String description, int imageResourceId) {
         this.name = name;
         this.description = description;
